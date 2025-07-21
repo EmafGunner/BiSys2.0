04d49747d814736698e343aab4c11c5fa1b42ad6
Emanuel Fedinich <emanuel.fedinich@gmail.com>
2025-07-21 20:47:23 -0300

    Pull de la Rama Developer Julio

--- bisys/src/routes/AppRouter.jsx
+++ bisys/src/routes/AppRouter.jsx
@@ -1,42 +1,47 @@
-import { Routes, Route } from 'react-router-dom'
-import HomeAdmin from '../pages/HomeAdmin'
-import HomeOperario from '../pages/HomeOperario'
-import HomeSupervisor from '../pages/HomeSupervisor'
-import Canos from '../pages/Canos'
-import Productos from '../pages/Productos'
-import Scrap from '../pages/Scrap'
-import OrdenProduccion from '../pages/OrdenProduccion'
-import OrdenTrabajo from '../pages/OrdenTrabajo'
-import Reportes from '../pages/Reportes'
+import { Routes, Route } from 'react-router-dom';
+
+import Inicio from '../pages/Inicio'; // Importá la nueva página de inicio
+import IniciarSesion from '../pages/IniciarSesion';
+import HomeAdmin from '../pages/HomeAdmin';
+import HomeEncargado from '../pages/HomeEncargado';
+import HomeOperario from '../pages/HomeOperario';
+
+import Canos from '../pages/Canos';
+import Productos from '../pages/Productos';
+import Scrap from '../pages/ScrapSeleccion'; 
+import ScrapCanos from '../pages/ScrapCanos';
+import ScrapProductos from '../pages/ScrapProductos';
+import OrdenProduccion from '../pages/OrdenProduccion';
+import OrdenTrabajo from '../pages/OrdenTrabajo';
+import Reportes from '../pages/Reportes';
 
 const AppRouter = () => {
-  // Más adelante vas a reemplazar este "userType" por uno traído desde la BD o login
-  const userType = 'admin' // 'operario', 'supervisor'
-
-  const renderHome = () => {
-    switch (userType) {
-      case 'admin':
-        return <HomeAdmin />
-      case 'operario':
-        return <HomeOperario />
-      case 'supervisor':
-        return <HomeSupervisor />
-      default:
-        return <div>Usuario desconocido</div>
-    }
-  }
-
   return (
     <Routes>
-      <Route path="/home" element={renderHome()} />
+      <Route path="/" element={<Inicio />} />
+      
+      {/* Página de inicio general */}
+      <Route path="/inicio" element={<Inicio />} />
+
+      {/* Página de logueo */}
+      <Route path="/iniciar-sesion" element={<IniciarSesion />} />
+
+      {/* Rutas para cada tipo de usuario */}
+      <Route path="/admin" element={<HomeAdmin />} />
+      <Route path="/encargado" element={<HomeEncargado />} />
+      <Route path="/operario" element={<HomeOperario />} />
+
+      {/* Módulos compartidos */}
       <Route path="/canos" element={<Canos />} />
       <Route path="/productos" element={<Productos />} />
       <Route path="/scrap" element={<Scrap />} />
       <Route path="/orden-produccion" element={<OrdenProduccion />} />
       <Route path="/orden-trabajo" element={<OrdenTrabajo />} />
+      <Route path="/scrap-canos" element={<ScrapCanos />} />
+      <Route path="/scrap-productos" element={<ScrapProductos />} />
       <Route path="/reportes" element={<Reportes />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
