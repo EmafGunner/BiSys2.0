6e5c91f84a8c9ec638869ecd87282bf9eb519d51
Emanuel Fedinich <emanuel.fedinich@gmail.com>
2025-09-15 22:40:19 -0300

    Formateo de PC y Clonación de Repo

--- bisys/src/routes/approuter.jsx
+++ bisys/src/routes/approuter.jsx
@@ -1,80 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
-
-import HomeAdmin from '../pages/HomeAdmin';
-import HomeEncargado from '../pages/HomeEncargado';
-import HomeOperario from '../pages/HomeOperario';
-
-import Canos from '../pages/Canos';
-import Productos from '../pages/Productos';
-import Scrap from '../pages/ScrapSeleccion'; 
-import ScrapCanos from '../pages/ScrapCanos';
-import ScrapProductos from '../pages/ScrapProductos';
-import OrdenProduccion from '../pages/OrdenProduccion';
-import OrdenTrabajo from '../pages/OrdenTrabajo';
-import Reportes from '../pages/Reportes';
-
-import Ingreso from '../pages/Ingreso';
-import IniciarSesion from '../pages/IniciarSesion';
-
-
-
-
-
-
+import { Routes, Route } from 'react-router-dom'
+import HomeAdmin from '../pages/HomeAdmin'
+import HomeOperario from '../pages/HomeOperario'
+import HomeSupervisor from '../pages/HomeSupervisor'
+import Canos from '../pages/Canos'
+import Productos from '../pages/Productos'
+import Scrap from '../pages/Scrap'
+import OrdenProduccion from '../pages/OrdenProduccion'
+import OrdenTrabajo from '../pages/OrdenTrabajo'
+import Reportes from '../pages/Reportes'
 
 const AppRouter = () => {
-  // Más adelante vas a reemplazar este "userType" por uno traído desde la BD o Login.
-  const userType = 'admin'; // 'operario', 'encargado'
+  // Más adelante vas a reemplazar este "userType" por uno traído desde la BD o login
+  const userType = 'admin' // 'operario', 'supervisor'
 
   const renderHome = () => {
     switch (userType) {
       case 'admin':
-        return <HomeAdmin />;
+        return <HomeAdmin />
       case 'operario':
-        return <HomeOperario />;
-      case 'encargado':
-        return <HomeEncargado />;
+        return <HomeOperario />
+      case 'supervisor':
+        return <HomeSupervisor />
       default:
-        return <div>Usuario desconocido</div>;
+        return <div>Usuario desconocido</div>
     }
-  };
+  }
 
   return (
     <Routes>
-      {/* Ruta dinámica principal */}
-      <Route path="/" element={renderHome()} />
-
-      {/* Rutas directas para testeo manual */}
-      <Route path="/admin" element={<HomeAdmin />} />
-      <Route path="/encargado" element={<HomeEncargado />} />
-      <Route path="/operario" element={<HomeOperario />} />
-
-      {/* Módulos compartidos */}
+      <Route path="/home" element={renderHome()} />
       <Route path="/canos" element={<Canos />} />
       <Route path="/productos" element={<Productos />} />
       <Route path="/scrap" element={<Scrap />} />
       <Route path="/orden-produccion" element={<OrdenProduccion />} />
       <Route path="/orden-trabajo" element={<OrdenTrabajo />} />
-     
-      <Route path="/scrap-canos" element={<ScrapCanos />} />
-      <Route path="/scrap-productos" element={<ScrapProductos />} />
-
-    
       <Route path="/reportes" element={<Reportes />} />
-
-
-
-      <Route path="/ingreso" element={<Ingreso />} />
-      <Route path="/login" element={<IniciarSesion />} />
-
-
-
-
-
-  
-
     </Routes>
-  );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
